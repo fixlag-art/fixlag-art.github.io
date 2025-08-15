b48371483015ddffaa31e1106ec5c9367c647c35
SHOICHI.SATO <shoichi.sato@fixlag.net>
2025-08-15 11:20:05 +0900

    Delete fixlag-art.github.io directory

--- fixlag-art.github.io/assets/common.js
+++
@@ -1,8 +0,0 @@
-// 左ナビ＆サブナビ：現在地ハイライト
-(function(){
-  const here = location.pathname.replace(/\/+$/,'').toLowerCase();
-  document.querySelectorAll('.sidenav a, .subnav a').forEach(a=>{
-    const p = new URL(a.getAttribute('href'), location.origin).pathname.replace(/\/+$/,'').toLowerCase();
-    if(here === p || (p !== '/' && here.startsWith(p+'/'))) a.classList.add('active');
-  });
-})();
